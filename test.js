a1495d407f46cd0c0d7c4868d2e89f20a2e7f5ac
alextremmel <alextremmel@gmail.com>
2023-10-10 11:32:42 -0400

    te

--- test.js
+++ test.js
@@ -2,9 +2,5 @@ const calId = process.env.CAL_ID;
 const clientEmail = process.env.CL_EMAIL;
 const privateKey = process.env.PRIV_KEY;
 
-if (calId && clientEmail && privateKey) {
-  console.log(`Calendar ID: ${calId}`);
-  console.log(`Client Email: ${clientEmail}`);
-} else {
-  console.error('One or more secret variables not found.');
-}
+console.log(`CAL_ID: ${calId}`);
+console.log(`CL_EMAIL: ${clientEmail}`);
